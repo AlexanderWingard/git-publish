68c74c42a1d8236fa104ddce58c0189ae825382c
Alexander Wingård <alexander.wingard@gmail.com>
2016-05-03 16:03:53 +0200

    Don't use shpy for tests
    This caused problems with IO-redrection and logging and stuff.

--- tests.py
+++ tests.py
@@ -3,17 +3,19 @@ from gerrit_publish import *
 import argparse
 import os
 import shlex
+import subprocess
 import sys
 import tempfile
 import unittest
+
+rootlogger = logging.getLogger()
+rootlogger.setLevel(logging.DEBUG)
 
 
 class TestPublish(unittest.TestCase):
     def setUp(self):
         self.oldargv = sys.argv
         self.oldcwd = os.getcwd()
-        sys.stdout = StringIO()
-        sys.stderr = sys.stdout
 
     def tearDown(self):
         sys.argv = self.oldargv
@@ -22,11 +24,11 @@ class TestPublish(unittest.TestCase):
     def test_simple_publish(self):
         self.given_test_repo()
         self.make_commit()
-        my_commit = c("git rev-parse HEAD")[0]
+        my_commit = self.run_command("git rev-parse HEAD")[0]
         out = self.run_publish("origin/master")
-        first_parent = c("git rev-parse HEAD^")[0]
-        back_merge = c("git rev-parse HEAD")[0]
-        second_parent = c("git rev-parse HEAD^2")[0]
+        first_parent = self.run_command("git rev-parse HEAD^")[0]
+        back_merge = self.run_command("git rev-parse HEAD")[0]
+        second_parent = self.run_command("git rev-parse HEAD^2")[0]
         self.assertNotEqual(my_commit, back_merge)
         self.assertNotEqual(first_parent, second_parent)
         self.assertEqual(my_commit, first_parent)
@@ -47,7 +49,7 @@ class TestPublish(unittest.TestCase):
         self.assertNotIn("error: ", out, "Error in --help:\n{}".format(out))
 
     def make_commit(self):
-        c("git commit --allow-empty -m test")
+        self.run_command("git commit --allow-empty -m test")
 
     def given_test_repo(self):
         test_data = "{}/test_data".format(os.getcwd())
@@ -55,14 +57,20 @@ class TestPublish(unittest.TestCase):
             os.makedirs(test_data)
         repo_dir = tempfile.mkdtemp(dir=test_data)
         os.chdir(repo_dir)
-        c("git init")
-        c("git config user.name 'gerrit publish'")
-        c("git config user.email 'gerrit@publish.com'")
-        c("git commit --allow-empty -m 'Initial commit'")
-        self.assertEqual(repo_dir, c("git rev-parse --show-toplevel", exit=True)[0])
+        self.run_command("git init")
+        self.run_command("git config user.name 'gerrit publish'")
+        self.run_command("git config user.email 'gerrit@publish.com'")
+        self.run_command("git commit --allow-empty -m 'Initial commit'")
+        self.assertEqual(repo_dir, self.run_command("git rev-parse --show-toplevel"))
         return repo_dir
 
     def run_publish(self, args, should_crash=False):
+        oldstdout = sys.stdout
+        oldstderr = sys.stderr
+        sys.stdout = StringIO()
+        sys.stderr = sys.stdout
+
+
         sys.stdout.seek(0)
         splitted = shlex.split(args)
         sys.argv = ["gerrit_publish"] + splitted
@@ -73,8 +81,14 @@ class TestPublish(unittest.TestCase):
             main()
 
         ret = sys.stdout.getvalue().strip()
+        sys.stdout = oldstdout
+        sys.stderr = oldstderr
+
         return ret
 
+    def run_command(self, command):
+        with open(os.devnull, 'w') as shutup:
+            return subprocess.check_output(shlex.split(command)).strip()
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
